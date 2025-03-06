eaa5c3e9f37655e4de4e9200e61a99c4ebff188d
Vitor Hugo <140010807+vitorhugo8@users.noreply.github.com>
2025-03-06 15:51:52 -0300

    ...

---
+++ Aula4/aula4.py
@@ -0,0 +1,13 @@
+"""
+Closure - É um objeto de função que lembra dos valores nos escopos envolventes.
+
+"""
+def saudacao(saudacao, nome):
+    def saudar():
+        return f"{saudacao}, {nome}"
+    return saudar()
+
+
+s1 = saudacao("Bom dia", "Maria")
+print(s1)
+
